e7494913a6c069157e928e0f2c60f2dab2f0479b
Marcio <marcio@kazale.com>
2016-12-26 20:25:01 -0200

    browserify

--- karma.conf.js
+++ karma.conf.js
@@ -10,12 +10,13 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'browserify'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      'spec/**/*Spec.js'
+      'spec/**/*Spec.js',
+      'spec/helpers/**/SpecHelper.js'
     ],
 
 
@@ -27,6 +28,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'spec/**/*Spec.js': ['browserify']
     },
 
 
